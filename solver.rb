f8ac420c78b572e1982884f0867bce626f5ceaa4
Rory O’Kane <rory@roryokane.com>
2012-11-21 15:01:25 -0500

    split blank puzzle and solution

--- solver.rb
+++ solver.rb
@@ -19,17 +19,47 @@ class Line
 	end
 end
 
+# a blank puzzle – just the empty grid and the numbers in the border
 class Puzzle
-	attr_reader :row_runs, :column_runs, :grid
+	attr_reader :row_runs, :column_runs
 	
 	def initialize(row_runs, column_runs)
-		
-		@grid = Array.new(row_runs.size, Array.new(column_runs.size, Square.new))
+		@row_runs = row_runs
+		@column_runs = column_runs
 	end
 	
 	def all_runs
 		@row_runs + @column_runs
 	end
+	
+	def to_s
+		
+	end
+end
+
+# a puzzle’s solution in progress – a grid with squares unknown, filled in or ruled out
+class SolutionAttempt
+	attr_reader :grid
+	
+	def initialize()
+		@grid = Array.new(row_runs.size, Array.new(column_runs.size, Square.new))
+		# TODO check for duplication bugs – do I need to pass a proc instead of an
+		#  Array literal to the outer array, or does Array #clone them for me?
+	end
+	
+	def [](row, column)
+		
+	end
+	
+	def fill_in(row, column, state)
+		
+	end
+	alias :[]= :fill_in
+	
+	def complete?
+		
+	end
+	alias :solved? :complete?
 	
 	def to_s
 		
